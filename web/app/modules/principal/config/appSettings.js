bcdbb82ae1844142b855363079d3ed7f9462c6df
donatoressa <donatoressa@gmail.com>
2017-09-06 18:06:05 -0300

    Modificação da url da api

--- web/app/modules/principal/config/appSettings.js
+++ web/app/modules/principal/config/appSettings.js
@@ -5,7 +5,8 @@
 
     angular.module("petquest.principal").constant("appSettings", {
         comunicacao: {
-            apis: "http://localhost:3000"
+            // apis: "http://localhost:3000"
+            apis: "http://petquest-api.firebaseapp.com"
         }
     });
 
